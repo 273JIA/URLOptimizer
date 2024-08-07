f0db1fa4ff50b8ff87cb94e3b9cb3b428cd110bf
Jia273 <114813337+01226@users.noreply.github.com>
2024-08-07 09:11:21 +0100

    add user query function,add Phone Desensitization Serializer

--- admin/src/main/java/com/coding/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/coding/shortlink/admin/service/UserService.java
@@ -1,5 +1,6 @@
 package com.coding.shortlink.admin.service;
 
+
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.coding.shortlink.admin.dao.entity.UserDO;
 import com.coding.shortlink.admin.dto.resp.UserRespDTO;
@@ -8,6 +9,7 @@ import com.coding.shortlink.admin.dto.resp.UserRespDTO;
  * 用户接口层
  */
 public interface UserService extends IService<UserDO> {
+
     /**
      * 根据用户名查询用户信息
      *
@@ -15,5 +17,13 @@ public interface UserService extends IService<UserDO> {
      * @return 用户返回实体
      */
     UserRespDTO getUserByUsername(String username);
+
+    /**
+     * 查询用户名是否存在
+     *
+     * @param username 用户名
+     * @return 用户名存在返回 True，不存在返回 False
+     */
+    Boolean hasUsername(String username);
 
 }
